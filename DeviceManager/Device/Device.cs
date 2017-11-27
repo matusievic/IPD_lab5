57d19cef029fa0ebdaeccf636343901faf754760
Andrei Matusevich <>
2017-11-27 15:52:14 +0200

    Device class was added

---
+++ DeviceManager/Device/Device.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeviceManager.Device
+{
+    class Device
+    {
+        public string GUID { get; set; }
+        public string HardwareID { get; set; }
+        public string Manufacturer { get; set; }
+        public string Provider { get; set; }
+        public string DriverDescription { get; set; }
+        public string SysFile { get; set; }
+        public string DevicePath { get; set; }
+    }
+}
